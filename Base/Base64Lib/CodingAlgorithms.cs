ce29e27848b22258d3091adc3a664116f19abb81
Aloana <62552242+dieskipy@users.noreply.github.com>
2020-06-19 11:00:38 +0300

    Add files via upload

---
+++ Base/Base64Lib/CodingAlgorithms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64Lib
+{
+    [PluginType(".base64")]
+    public class CodingAlgorithms
+    {
+        public static void Encode(string filePath)
+        {
+            string mystr = "";
+            using (StreamReader br = new StreamReader(filePath,Encoding.UTF8))
+            {
+                mystr = br.ReadToEnd();
+            }
+
+            byte[] toEncodeAsBytes= Encoding.UTF8.GetBytes(mystr);
+
+            mystr = Convert.ToBase64String(toEncodeAsBytes);
+            File.Delete(filePath);
+            filePath += ".base64";
+
+            using (StreamWriter sr = new StreamWriter(filePath,false,Encoding.UTF8))
+            {
+                sr.Write(mystr);
+            }
+        }
+
+        public static void Decode(string filePath)
+        {
+            string mystr = "";
+            using (StreamReader br = new StreamReader(filePath, Encoding.UTF8))
+            {
+                mystr = br.ReadToEnd();
+            }
+            byte[] encodedDataAsBytes = Convert.FromBase64String(mystr);
+            mystr = Encoding.UTF8.GetString(encodedDataAsBytes);
+            
+            File.Delete(filePath);
+            filePath = filePath.Substring(0, filePath.Length - 7);
+
+            using (StreamWriter sr = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                sr.Write(mystr);
+            }
+        }
+
+    }
+
+}
